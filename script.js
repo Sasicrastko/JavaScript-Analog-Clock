13488696fc5fc2574341d4e630d51cf9d9c49cbe
Rastko Sasic <rsasic@europe.com>
2018-08-09 23:31:30 -0400

    fixing hour angle formula

--- script.js
+++ script.js
@@ -2,23 +2,20 @@ var secondHand = document.getElementById("second-hand");
 var minuteHand = document.getElementById("minute-hand");
 var hourHand = document.getElementById("hour-hand");
 
-
-
 function moveHands(){
   var d = new Date();
   var sec = d.getSeconds();
   var min = d.getMinutes();
-  var hour = d.getHours()%12; //only 12 hours on analog watch
+  var hour = d.getHours()%12; // % needed because there is only 12 hours on analog watch
 
   var secondHandAngle = (sec*360)/60-90; //-90 because CSS set hands horizontaly when angle is 0deg
   var minuteHandAngle = (min*360)/60-90;
-  var hourHandAngle = (hour*360)/12-90;
+  var hourHandAngle = (hour*360)/12+(min*30)/60-90; //(min*30)/60 added simulate real analog watch
 
   secondHand.style.transform = `rotate(${secondHandAngle}deg)`;
   minuteHand.style.transform = `rotate(${minuteHandAngle}deg)`;
   hourHand.style.transform = `rotate(${hourHandAngle}deg)`;
 }
-
 
 setInterval(function(){moveHands();}, 1000);
 
