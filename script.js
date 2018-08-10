2f6bd5b2b0a7432ff0b936599d12d1a38eb1cb55
Rastko Sasic <rsasic@europe.com>
2018-08-09 22:52:45 -0400

    moving the hand that tells seconds

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ var secondHand = document.getElementById("second-hand");
 var minuteHand = document.getElementById("minute-hand");
 var hourHand = document.getElementById("hour-hand");
 
-var a = 0;
+
 
 function moveHands(){
-  a++;
-  b++;
-  c++;
-  secondHand.style.transform = `rotate(${a}deg)`;
+  var d = new Date();
+  var sec = d.getSeconds();
+  var min = d.getMinutes();
+  var hour = d.getHours()%12;
+
+  var secondHandAngle = (sec*360)/60;
+
+  secondHand.style.transform = `rotate(${secondHandAngle}deg)`;
+  minuteHand.style.transform = `rotate(${minuteHandAngle}deg)`;
+  hourHand.style.transform = `rotate(${hourHandAngle}deg)`;
 }
 
 
-setInterval(function(){ moveHands(); }, 1000);
+setInterval(function(){moveHands();}, 1000);
 
